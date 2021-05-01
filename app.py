82a61ecfb3ab31f0ccd73edde78d39b7432f50d9
rekhajain-jaipur <83448705+rekhajain-jaipur@users.noreply.github.com>
2021-05-01 07:29:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Apr 27 10:56:22 2021
 
-@author: deepak
+@author: rekha
 """
 
 import streamlit as st 
@@ -13,20 +13,24 @@ import matplotlib.pyplot as plt
 import pandas as pd
 st.set_option('deprecation.showfileUploaderEncoding', False)
 # Load the pickled model
-model = pickle.load(open('logisticmodel.pkl', 'rb')) 
-# Feature Scaling
-dataset = pd.read_csv('Social_Network_Ads.csv')
-# Extracting independent variable:
-X = dataset.iloc[:, [1,2,3]].values
-# Encoding the Independent Variable
-from sklearn.preprocessing import LabelEncoder
-labelencoder_X = LabelEncoder()
-X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+model = pickle.load(open('svmmodel.pkl','rb')) 
+model_randomforest = pickle.load(open('randomforest.pkl', 'rb')) 
+dataset= pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X = sc.fit_transform(X)
 def predict_note_authentication(UserID, Gender,Age,EstimatedSalary):
-  output= model.predict(sc.transform([[Gender,Age,EstimatedSalary]]))
+  output= model.predict(sc.transform([[Age,EstimatedSalary]]))
+  print("Purchased", output)
+  if output==[1]:
+    prediction="Item will be purchased"
+  else:
+    prediction="Item will not be purchased"
+  print(prediction)
+  return prediction
+def predict_random(UserID, Gender,Age,EstimatedSalary):
+  output= model_randomforest.predict(sc.transform([[Age,EstimatedSalary]]))
   print("Purchased", output)
   if output==[1]:
     prediction="Item will be purchased"
@@ -37,34 +41,46 @@ def predict_note_authentication(UserID, Gender,Age,EstimatedSalary):
 def main():
     
     html_temp = """
-   <div class="" style="background-color:Brown;" >
+   <div class="" style="background-color:blue;" >
    <div class="clearfix">           
    <div class="col-md-12">
-   <center><p style="font-size:40px;color:black;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center> 
-   <center><p style="font-size:30px;color:black;margin-top:10px;">Department of Computer Engineering</p></center> 
-   <center><p style="font-size:25px;color:black;margin-top:10px;"Machine Learning Lab Experiment</p></center> 
+   <center><p style="font-size:40px;color:white;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center> 
+   <center><p style="font-size:30px;color:white;margin-top:10px;">Department of Computer Engineering</p></center> 
+   <center><p style="font-size:25px;color:white;margin-top:10px;"Machine Learning Lab Experiment</p></center> 
    </div>
    </div>
    </div>
    """
     st.markdown(html_temp,unsafe_allow_html=True)
-    st.header("Item Purchase Prediction using Logistic Classification")
+    st.header("Item Purchase Prediction using SVM Algorithm")
+    UserID = st.text_input("UserID","Type Here")
+    Gender = st.selectbox(
+    "Gender",
+    ("Male", "Female", "Others")
+    )
     
-    UserID = st.text_input("UserID","")
-    
-    #Gender1 = st.select_slider('Select a Gender Male:1 Female:0',options=['1', '0'])
-    Gender1 = st.number_input('Insert Gender Male:1 Female:0')
-    Age = st.number_input('Insert a Age',18,60)
-   
-    EstimatedSalary = st.number_input("Insert Estimated Salary",15000,150000)
+    Age = st.number_input('Insert a Age',0,100)
+    #Age = st.text_input("Age","Type Here")
+    EstimatedSalary = st.number_input("Insert EstimatedSalary",1000,1000000000)
     resul=""
-    if st.button("Predict"):
-      result=predict_note_authentication(UserID, Gender1,Age,EstimatedSalary)
-      st.success('Model has predicted {}'.format(result))
-      
+    if st.button("SVM Prediction"):
+      result=predict_note_authentication(UserID, Gender,Age,EstimatedSalary)
+      st.success('SVM Model has predicted {}'.format(result))
+    if st.button("Random Forest Prediction"):
+      result=predict_random(UserID, Gender,Age,EstimatedSalary)
+      st.success('Random forest Model  has predicted {}'.format(result))  
     if st.button("About"):
-      st.subheader("Developed by Deepak Moud")
-      st.subheader("Head , Department of Computer Engineering")
-
+      st.header("Developed by Rekha Jain")
+      st.subheader("Professor , Department of Computer Engineering")
+    html_temp = """
+    <div class="" style="background-color:orange;" >
+    <div class="clearfix">           
+    <div class="col-md-12">
+    <center><p style="font-size:20px;color:white;margin-top:10px;">Machine Learning Experiment 5: Support Vector Machine and Random Forest</p></center> 
+    </div>
+    </div>
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
 if __name__=='__main__':
   main()
